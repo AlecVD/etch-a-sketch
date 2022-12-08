77e2ab348de7c3d72e0b9b1771e382ceed9e1136
alecvd <14879968+AlecVD@users.noreply.github.com>
2022-12-07 20:12:47 -0800

    randomize color

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 var container = document.getElementById("container")
-var rows = 5
-var columns = 5
+var rows = 2
+var columns = 2
 
 for(var i = 0; i < rows; i++){
     var row = document.createElement("div")
     row.classList.add('heads')
     for(var j = 0; j < columns; j++){
         var element = document.createElement("div")
-        element.textContent = "element"
+        var index = j
         element.classList.add("element")
+        element.addEventListener("mouseover",changeColor)
         row.append(element)
     }
     container.appendChild(row)
+}
+
+function changeColor(e){
+    var r = Math.floor(Math.random() * 256)
+    var g = Math.floor(Math.random() * 256)
+    var b = Math.floor(Math.random() * 256)
+    console.log(r)
+    e.target.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")"
 }
